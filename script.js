d2c94d0d0aa6a152a1ac525f93defda0c5549bd7
Elijah Mbugi <mukundielijah@gmail.com>
2022-03-06 15:25:33 +0300

    Define variables

---
+++ script.js
@@ -0,0 +1,8 @@
+function akanName(){
+    var DD = parseInt(document.getElementById("day").value);
+    var MM = parseInt(document.getElementById("month").value);
+    var year = document.getElementById("year").value;
+    var CC = parseInt(year.slice(0,2));
+    var YY = parseInt(year.slice(2,4));
+    // console.log(CC);
+};
