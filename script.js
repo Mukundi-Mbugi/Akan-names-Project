06c32c5ecaecabc65e292f3e85bd05c6559ed063
Elijah Mbugi <mukundielijah@gmail.com>
2022-03-06 16:08:09 +0300

    Add error statement for wrong date entered

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ function akanName(){
     var CC = parseInt(year.slice(0,2));
     var YY = parseInt(year.slice(2,4));
     // console.log(CC);
+
+    if(DD<1||DD>31){
+        document.getElementById("alert1").innerHTML="Error! Enter a valid date.";
+        };
 };
